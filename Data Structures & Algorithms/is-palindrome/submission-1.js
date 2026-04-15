45e8a440b38a5a9b006e5b09107fa60996778930
ratmirj <ratmir.junusow@gmail.com>
2026-04-15 23:41:05 +0200

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        const news = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+        let start = 0;
+        let end = news.length - 1;
+
+        while (end > start){
+            if(news[start] !== news[end]) return false;
+            start++;
+            end--;
+        }
+
+        return true
+    }
+}
