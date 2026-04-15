1657a069191a02a6ac01803902874cd5ddc623e8
ratmirj <ratmir.junusow@gmail.com>
2026-04-16 01:40:40 +0200

    Add: text-justification - submission-2

---
+++ Data Structures & Algorithms/text-justification/submission-2.js
@@ -0,0 +1,45 @@
+class Solution {
+    /**
+     * @param {string[]} words
+     * @param {number} maxWidth
+     * @return {string[]}
+     */
+    
+fullJustify(words, maxWidth) {
+    const result = [];
+    let i = 0;
+
+    while (i < words.length) {
+        let line = [];
+        let lineLength = 0;
+
+        while (i < words.length && lineLength + words[i].length + line.length <= maxWidth) {
+            line.push(words[i]);
+            lineLength += words[i].length;
+            i++;
+        }
+
+        const leftJustified = i === words.length || line.length === 1;
+
+        if (leftJustified) {
+            result.push(line.join(' ') + ' '.repeat(maxWidth - (lineLength + line.length - 1)));
+        } else {
+            const totalSpaces = maxWidth - lineLength;
+            const gaps = line.length - 1;
+            const spacePerGap = Math.floor(totalSpaces / gaps);
+            const extraSpaces = totalSpaces % gaps;
+
+            let formatted = '';
+            for (let j = 0; j < line.length; j++) {
+                formatted += line[j];
+                if (j < gaps) {
+                    formatted += ' '.repeat(spacePerGap + (j < extraSpaces ? 1 : 0));
+                }
+            }
+            result.push(formatted);
+        }
+    }
+
+    return result;
+}
+}
